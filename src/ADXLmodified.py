ff00f8e618f78ad845c08be970bfaff4b745b5ce
Aniket Rege <aniketrg7@gmail.com>
2016-10-13 02:07:30 +0530

    Proper note triggering functionality and comments added

--- src/ADXLmodified.py
+++ src/ADXLmodified.py
@@ -4,7 +4,6 @@ import serial
 import rtmidi_python as rtmidi
 import time
 
-
 midi_out = rtmidi.MidiOut()
 midi_out.open_port(0)
 
@@ -23,21 +22,25 @@ ser = serial.Serial(com_port, 9600)
 #ser.reset_input_buffer()
 
 
-
+OFF=0x80
+ON=0x90
+PAN=0xB0
 accelerometer_range=23.0    #range of values which adxl sends = 11 - (-11) +1
 no_bins=len(argv)-2         #remove script name and com port number
 bin_size = accelerometer_range/no_bins 
-decision_boundary=[]       
+decision_boundary=[]
+time.sleep(1)
+off=0
+on=1
+pan_note=0
+previousnote=0
+
 for i in range(0,no_bins):
     decision_boundary.append((-11+(bin_size*i),-11+(bin_size*(i+1)),i))  #list of all boundaries according to number of notes
 print "number of bins- " + str(no_bins)
 print "bin size- " + str(bin_size)
 print "Decision Boundaries- " + str(decision_boundary)
-time.sleep(1)
-off=0
-on=1
-previousnote1=0
-previousnote2=0
+
 while(1):
     #ser.write('t')  #activate Arduino to send adxl values
     reading = ser.readline().rstrip()
@@ -49,33 +52,40 @@ while(1):
     #print xreading
     #print count
     for (lower,upper,index) in decision_boundary:
-        if(count%2==0):
+        current_note= (notes[index])
+        if(count%2==0): 
+
+        #********       MODE ONE        ********
+
             if(lower<=zreading<upper):
                 if(off==1):
-                    midi_out.send_message([0x80,previousnote1,100])
-                if(previousnote2!=int(notes[index])):
+                    midi_out.send_message([OFF,pan_note,100])       #if mode switched from pan, switch off pan note
+                if(previousnote!=int(current_note)):                 #if note is different from last, switch it off
                     on=1
-                    midi_out.send_message([0x80,previousnote2,100])
+                    midi_out.send_message([OFF,previousnote,100])
                 if(on==1):
-                    print "Playing note- " + str((notes[index]))                #display which note is playing
-                    previousnote2=int(notes[index])
-                    midi_out.send_message([0x90, int(notes[index]),100])        #this will play note corresponding to whichever bin input is in
-                on=0
-                time.sleep(0.3)
-                midi_out.send_message([0x80, int(notes[index]),100])
-                time.sleep(0.1)
-                break
-        else:
+                    print "Playing note- " + str((current_note))                #display which note is playing
+                    previousnote=int(current_note)                              #store previous note for next iteration to check
+                    midi_out.send_message([ON, int(current_note),100])        #this will play note corresponding to whichever bin input is in
+                    on=0
+                    time.sleep(0.3)
+                    midi_out.send_message([OFF, int(current_note),100])            #switch off current note
+                    time.sleep(0.1)
+                break   
+        else:                                       
+
+        #********       MODE TWO        ********
+
             if(lower<=zreading<upper):
-                print "holding note- " + str((notes[index]))
+                print "holding note- " + str((current_note))
                 print "panvalue ----"
-                print xreading      #display which note is playing
+                print xreading                    #display which note is playing
                 if(off==0):
-                       previousnote1=int(notes[index])
-                       midi_out.send_message([0x90, int(notes[index]),100])
+                       pan_note=int(current_note)
+                       midi_out.send_message([ON, int(current_note),100])     #This will be switched off only if mode changes
                 time.sleep(0.3)
                 off=1
-                midi_out.send_message([0xB0,10,xreading])                   #pan
+                midi_out.send_message([PAN,10,xreading])                       #CC: Pan 
                 time.sleep(0.1)
                 break
             
@@ -86,4 +96,4 @@ while(1):
 
 #Terminal Commands:
 #cd C:\Users\Aniket\Documents\Projects\Technites\Glove 2.0 (navigate to where this code is locally)
-#python accelerometer_locking_pan.py COM3 60 62 65 67 72
+#python ADXLmodified.py COM3 60 62 65 67 72
