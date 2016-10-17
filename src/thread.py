f655e78af60a02bce71aba51a1fceee9dd1d63c7
Aniket Rege <aniketrg7@gmail.com>
2016-10-17 13:15:12 +0530

    Threading psuedocode

---
+++ src/thread.py
@@ -0,0 +1,16 @@
+from threading import Thread
+
+#func(arg)
+#{
+#function body
+#}
+
+#main loop()
+#{
+#loop body
+
+#variable=Thread(target=func, args= arg)
+#variable.start()
+
+#loop body continues
+#}
